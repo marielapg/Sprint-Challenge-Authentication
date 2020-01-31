f1603cdbafc813aefc058c69bd5bdc936ba36ba6
marielapg <mpgonzales77@gmail.com>
2020-01-31 08:20:02 -0800

    installed dependencies and testing environment

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,17 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  testing: {
+    client: "sqlite3",
+    connection: { filename: "./databae/test.db3", },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: { directory: "./database/migrations", },
+    seeds: { directory: "./database/seeds", },
+  }
 };
+
